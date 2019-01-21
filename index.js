d01f2ab1a6e7732d536fbe5205bc43977235e550
Igor Yegorov <iegoroff@gmail.com>
2019-01-21 05:16:13 +0300

    jest fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import tempfile from 'tempfile'
-import { readFile } from 'fs'
-import { promisify } from 'util'
-import AndroidDevicePathBuilder from 'detox/src/artifacts/utils/AndroidDevicePathBuilder'
+const tempfile = require('tempfile')
+const { readFile } = require('fs')
+const { promisify } = require('util')
+const AndroidDevicePathBuilder = require('detox/src/artifacts/utils/AndroidDevicePathBuilder')
 
 const read = promisify(readFile)
 
@@ -9,7 +9,7 @@ const devicePathBuilder = new AndroidDevicePathBuilder()
 
 const isIos = () => device._deviceConfig.type.startsWith('ios')
 
-export const screenshot = async () => {
+const screenshot = async () => {
   const { deviceDriver: { adb, _applesimutils: appleSimUtils }, _deviceId: deviceId } = device
   const temp = tempfile('.png')
 
@@ -26,4 +26,6 @@ export const screenshot = async () => {
   return read(temp)
 }
 
-export const identifier = (name) => `${isIos() ? 'ios' : 'android'}-${name}`
+const identifier = (name) => `${isIos() ? 'ios' : 'android'}-${name}`
+
+module.exports = { screenshot, identifier }
